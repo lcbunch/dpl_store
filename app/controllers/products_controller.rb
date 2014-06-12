b1c574d96282e127b4c92573ef7eb2c831d76efe
lcbunch <lbunch@vt.edu>
2014-06-12 14:22:35 -0600

    add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,39 @@
 class ProductsController < ApplicationController
+#the index action
+#this should list all of our products
+#index.html.erb
+
+  def index
+    @products = Product.all
+  end
+
+  #This should find a particular product
+  #show.html.erb
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    #save to the database
+    #raise pauses program
+    @product = Product.new(product_params)
+    if @product.save
+    redirect_to products_path
+    else
+    render :new
+    end
+  end
+
+  private
+  #define a private method
+  #use strong parameters
+  def product_params
+    #params[:product]
+    params.require(:product).permit(:name)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-Product.create(name: 'DPL Shirt', price 14.99, sku: 'dp1123', description: 'An awesome purple shirt with the DPL logo')
-Product.create(name: 'DPL Mug', price 3.25, sku: 'dp1456', description: 'An awesome purple mug with the DPL logo')
+Product.create(name: 'DPL Shirt', price: 14.99, sku: 'dp1123', description: 'An awesome purple shirt with the DPL logo')
+Product.create(name: 'DPL Mug', price: 3.25, sku: 'dp1456', description: 'An awesome purple mug with the DPL logo')
