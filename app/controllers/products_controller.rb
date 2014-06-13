2311cb307fc6ac9a1ef53352f7a9d81fbb9f562c
lcbunch <lbunch@vt.edu>
2014-06-13 12:04:55 -0600

    homepage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 #the index action
 #this should list all of our products
 #index.html.erb
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
@@ -10,7 +11,7 @@ class ProductsController < ApplicationController
   #This should find a particular product
   #show.html.erb
   def show
-    @product = Product.find(params[:id])
+    #@product = Product.find(params[:id])
   end
 
   def new
@@ -22,10 +23,31 @@ class ProductsController < ApplicationController
     #raise pauses program
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = 'Good job! You did it.'
     redirect_to products_path
     else
+      flash[:alert] = 'Bad job! You failed.'
     render :new
     end
+  end
+
+  def edit
+    #@product = Product.find(params[:id])
+  end
+
+  def update
+    #@product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+    redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    #@product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   private
@@ -34,6 +56,10 @@ class ProductsController < ApplicationController
   def product_params
     #params[:product]
     params.require(:product).permit(:name)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 
 end
